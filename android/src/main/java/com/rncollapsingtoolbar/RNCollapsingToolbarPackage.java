0b0f5fbf50704407f59de87e07b363a792fd8c25
Cesar Augusto <cesardeazevedo@outlook.com>
2017-08-03 20:20:05 +0200

    Fixed compatibility with react-native@0.47.1

--- android/src/main/java/com/rncollapsingtoolbar/RNCollapsingToolbarPackage.java
+++ android/src/main/java/com/rncollapsingtoolbar/RNCollapsingToolbarPackage.java
@@ -18,11 +18,6 @@ public class RNCollapsingToolbarPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
         return Arrays.<ViewManager>asList(
                 new AppBarLayoutManager(),
