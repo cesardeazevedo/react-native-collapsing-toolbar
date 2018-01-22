a8261cb63e986cf726f8fc6101c13d35f1af0b82
Nguyen Duc Binh <binhnd@socicom.com.vn>
2018-01-22 16:57:14 +0700

    Update layout manually

--- android/src/main/java/com/rncollapsingtoolbar/AppBarLayoutView.java
+++ android/src/main/java/com/rncollapsingtoolbar/AppBarLayoutView.java
@@ -3,6 +3,7 @@ package com.rncollapsingtoolbar;
 import android.content.Context;
 import android.support.design.widget.AppBarLayout;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 
 public class AppBarLayoutView extends AppBarLayout {
     public AppBarLayoutView(Context context) {
@@ -15,4 +16,30 @@ public class AppBarLayoutView extends AppBarLayout {
 
         this.setLayoutParams(params);
     }
+
+    @Override
+    public void requestLayout() {
+        super.requestLayout();
+        post(measureAndLayout);
+
+        // Notify to parent CoordinatorLayoutView to update if exist
+        ViewParent parentView = this.getParent();
+        while (parentView != null) {
+            if (parentView instanceof CoordinatorLayoutView) {
+                parentView.requestLayout();
+                break;
+            }
+            parentView = parentView.getParent();
+        }
+    }
+
+    private final Runnable measureAndLayout = new Runnable() {
+        @Override
+        public void run() {
+            measure(
+                    MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.EXACTLY));
+            layout(getLeft(), getTop(), getRight(), getBottom());
+        }
+    };
 }
